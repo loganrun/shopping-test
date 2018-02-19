ec8ec15ea985d290ea1868ba35df5eefe6d959cc
loganrun <bigdog_41@hotmail.com>
2018-02-19 19:28:49 +0000

    final

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -58,24 +58,21 @@ describe('recipes', function(){
        
     });
     
-    // it('should update recipes on PUT', function() {
-    //   const updateData = {name: 'candy', ingredients: ['water', 'chocolate', 'marshmellows']};
-    //   return chai.request(app)
-    //   .get('/recipes')
-    //   .then(function(res){
-    //       updateData.id = res.body[0].id;
-    //       return chai.request(app)
-    //       .put(`/recipes/${updateData.id}`)
-    //       .send(updateData);
-    //   })
+    it('should update recipes on PUT', function() {
+      const updateData = {name: 'foo', ingredients: ['water', 'chocolate', 'marshmellows']};
+      return chai.request(app)
+      .get('/recipes')
+      .then(function(res){
+          updateData.id = res.body[0].id;
+          return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData)
+      })
        
-    //   .then(function(res){
-    //       expect(res).to.have.status(200);
-    //       expect(res).to.be.json;
-    //       expect(res.body).to.be.a('object');
-    //       expect(res.body).to.deep.equal(updateData);
-    //   });
-    // });
+      .then(function(res){
+          expect(res).to.have.status(204);
+      });
+    });
     
     it('should delete recipes on DELETE', function(){
        return chai.request(app)
